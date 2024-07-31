0dadf9525e5a6aaa644b3982027c85d6da768fdf
LuanaFerreiraFabrica <luana.ferreira@fabricadebares.com.br>
2024-07-31 14:53:44 -0300

    Fix(Despesas):Aparição dos orçamentos

--- pages/Despesas.py
+++ pages/Despesas.py
@@ -32,7 +32,15 @@ def main():
   lojas_selecionadas, data_inicio, data_fim = criar_seletores(lojasComDados, data_inicio_default, data_fim_default)
   st.divider()
 
-  Despesas = GET_DESPESAS()
+  # Despesas = GET_DESPESAS()
+  despesasDetalhadas = GET_DESPESAS()
+  despesasDetalhadas = filtrar_por_datas(despesasDetalhadas, data_inicio, data_fim, 'Data_Evento')
+  despesasDetalhadas = filtrar_por_classe_selecionada(despesasDetalhadas, 'Loja', lojas_selecionadas)
+
+  Despesas2 = GET_DESPESAS2().drop_duplicates()
+  Orcamentos = GET_ORCAMENTOS_DESPESAS().drop_duplicates()
+  Despesas = pd.merge(Despesas2, Orcamentos, on=['Primeiro_Dia_Mes', 'Loja', 'Plano_de_Contas', 'Class_Plano_de_Contas'], how='outer')
+  Despesas['Data_Evento'] = Despesas['Data_Evento'].fillna(Despesas['Primeiro_Dia_Mes'])
   Despesas = filtrar_por_datas(Despesas, data_inicio, data_fim, 'Data_Evento')
   Despesas = filtrar_por_classe_selecionada(Despesas, 'Loja', lojas_selecionadas)
   despesasConfig = config_despesas_por_classe(Despesas)
@@ -44,9 +52,9 @@ def main():
       st.subheader("Despesas:")
       st.dataframe(despesasConfigStyled, height=500, use_container_width=True, hide_index=True)
 
-  despesaDetalhada = config_despesas_detalhado(Despesas)
+  despesaDetalhadaConfig = config_despesas_detalhado(despesasDetalhadas)
 
-  classificacoes = preparar_dados_classe_selecionada(despesaDetalhada, 'Plano de Contas')
+  classificacoes = preparar_dados_classe_selecionada(despesaDetalhadaConfig, 'Plano de Contas')
 
   with st.container(border=True):
     col0, col1, col2 = st.columns([1, 15, 1])
@@ -56,15 +64,15 @@ def main():
         st.subheader("Despesas Detalhadas:")
       with col4:
         classificacoes_selecionadas = st.multiselect(label='Selecione Classificações', options=classificacoes)
-        despesaDetalhada = filtrar_por_classe_selecionada(despesaDetalhada, 'Plano de Contas', classificacoes_selecionadas)
+        despesaDetalhadaConfig = filtrar_por_classe_selecionada(despesaDetalhadaConfig, 'Plano de Contas', classificacoes_selecionadas)
       with col5:
-        fornecedores = preparar_dados_classe_selecionada(despesaDetalhada, 'Fornecedor')
+        fornecedores = preparar_dados_classe_selecionada(despesaDetalhadaConfig, 'Fornecedor')
         fornecedores_selecionados = st.multiselect(label='Selecione Fornecedores', options=fornecedores)
-        despesaDetalhada = filtrar_por_classe_selecionada(despesaDetalhada, 'Fornecedor', fornecedores_selecionados)
-      valorTotal = despesaDetalhada['Valor'].sum()
+        despesaDetalhadaConfig = filtrar_por_classe_selecionada(despesaDetalhadaConfig, 'Fornecedor', fornecedores_selecionados)
+      valorTotal = despesaDetalhadaConfig['Valor'].sum()
       valorTotal = format_brazilian(valorTotal)
-      despesaDetalhada = format_columns_brazilian(despesaDetalhada, ['Valor'])
-      st.dataframe(despesaDetalhada, height=500, use_container_width=True, hide_index=True)
+      despesaDetalhadaConfig = format_columns_brazilian(despesaDetalhadaConfig, ['Valor'])
+      st.dataframe(despesaDetalhadaConfig, height=500, use_container_width=True, hide_index=True)
       st.write('Valor Total = R$', valorTotal)
 
 if __name__ == '__main__':

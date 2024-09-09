65272e9762f851b1e0516743208cf54aef49ae47
LuanaFerreiraFabrica <luana.ferreira@fabricadebares.com.br>
2024-09-09 18:05:03 -0300

    more adjusts on the extratos bancarios query

--- pages/DRE.py
+++ pages/DRE.py
@@ -6,6 +6,7 @@ from utils.functions.cmv import *
 from utils.functions.dados_gerais import *
 from utils.components import *
 from utils.user import logout
+import openpyxl
 
 st.set_page_config(
   layout = 'wide',
@@ -19,3 +20,21 @@ if 'loggedIn' not in st.session_state or not st.session_state['loggedIn']:
   st.switch_page('Login.py')
 
 
+
+# Permite o upload do arquivo
+uploaded_file = st.file_uploader("Faça o upload do arquivo Excel", type=["xlsx"])
+
+# Verifica se um arquivo foi carregado
+if uploaded_file is not None:
+  # Carrega o arquivo Excel em um DataFrame
+  df = pd.read_excel(uploaded_file, sheet_name='NomeDaPlanilha')
+
+  # Exibe o conteúdo do DataFrame
+  st.write("Dados carregados:")
+  st.dataframe(df)
+
+  # Procura pela palavra "TOTAL" na coluna B e pega o valor ao lado na coluna C
+  total_row = df[df['B'] == 'TOTAL'].index[0]
+  valor_total = df.at[total_row, 'C']
+
+  st.write(f"O valor ao lado de 'TOTAL' é: {valor_total}")

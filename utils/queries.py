df47c67f36c61b17c7de89c040b7cf9c510d6883
LuanaFerreiraFabrica <luana.ferreira@fabricadebares.com.br>
2024-09-26 17:57:32 -0300

    Fix(insumos com pedido cmv; planilha conciliacao): Adição de novas categorias e mudancas nas fórmulas excel

--- utils/queries.py
+++ utils/queries.py
@@ -792,7 +792,30 @@ def GET_PERDAS_E_CONSUMO_AGRUPADOS():
 
 
 
-@st.cache_data
+# @st.cache_data
+# def GET_INSUMOS_BLUE_ME_COM_PEDIDO():
+#   return dataframe_query(f'''
+#   SELECT
+#     vbmcp.tdr_ID AS tdr_ID,
+#     vbmcp.ID_Loja AS ID_Loja,
+#     vbmcp.Loja AS Loja,
+#     vbmcp.Fornecedor AS Fornecedor,
+#     vbmcp.Doc_Serie AS Doc_Serie,
+#     vbmcp.Data_Emissao AS Data_Emissao,
+#     vbmcp.Valor_Liquido AS Valor_Liquido,
+#     vbmcp.Valor_Insumos AS Valor_Insumos,
+#     CAST(DATE_FORMAT(CAST(vbmcp.Data_Emissao AS DATE), '%Y-%m-01') AS DATE) AS Primeiro_Dia_Mes,
+#     ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Alimentos / virapc.Valor_Total_Insumos)), 2) AS Valor_Liq_Alimentos,
+#     ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Bebidas / virapc.Valor_Total_Insumos)), 2) AS Valor_Liq_Bebidas,
+#     ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Descartaveis_Higiene_Limpeza / virapc.Valor_Total_Insumos)), 2) AS Valor_Liq_Descart_Hig_Limp,
+#     ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Outros / virapc.Valor_Total_Insumos)), 2) AS Valor_Liq_Outros
+#   FROM
+#     View_BlueMe_Com_Pedido vbmcp
+#   LEFT JOIN View_Insumos_Receb_Agrup_Por_Categ virapc ON
+#     vbmcp.tdr_ID = virapc.tdr_ID;
+# ''')
+
+
 def GET_INSUMOS_BLUE_ME_COM_PEDIDO():
   return dataframe_query(f'''
   SELECT
@@ -803,17 +826,20 @@ def GET_INSUMOS_BLUE_ME_COM_PEDIDO():
     vbmcp.Doc_Serie AS Doc_Serie,
     vbmcp.Data_Emissao AS Data_Emissao,
     vbmcp.Valor_Liquido AS Valor_Liquido,
-    vbmcp.Valor_Insumos AS Valor_Insumos,
+    vbmcp.Valor_Insumos AS Valor_Cotacao,
     CAST(DATE_FORMAT(CAST(vbmcp.Data_Emissao AS DATE), '%Y-%m-01') AS DATE) AS Primeiro_Dia_Mes,
     ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Alimentos / virapc.Valor_Total_Insumos)), 2) AS Valor_Liq_Alimentos,
     ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Bebidas / virapc.Valor_Total_Insumos)), 2) AS Valor_Liq_Bebidas,
     ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Descartaveis_Higiene_Limpeza / virapc.Valor_Total_Insumos)), 2) AS Valor_Liq_Descart_Hig_Limp,
+    ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Gelo_Gas_Carvao_Velas / virapc.Valor_Total_Insumos)), 2) AS Valor_Gelo_Gas_Carvao_Velas,
+    ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Utensilios / virapc.Valor_Total_Insumos)), 2) AS Valor_Utensilios,
     ROUND((vbmcp.Valor_Liquido * (virapc.Valor_Outros / virapc.Valor_Total_Insumos)), 2) AS Valor_Liq_Outros
   FROM
     View_BlueMe_Com_Pedido vbmcp
   LEFT JOIN View_Insumos_Receb_Agrup_Por_Categ virapc ON
-    vbmcp.tdr_ID = virapc.tdr_ID;
+    vbmcp.tdr_ID = virapc.tdr_ID
 ''')
+
 
 @st.cache_data
 def GET_INSUMOS_BLUE_ME_SEM_PEDIDO():
@@ -1451,53 +1477,6 @@ def GET_AJUSTES_CONCILIACAO():
   FROM T_AJUSTES_CONCILIACAO tac
 ''')
 
-
-
-# def GET_DESPESAS_PENDENTES(data):
-#   # Formatando as datas para o formato de string com aspas simples
-#   dataStr = f"'{data.strftime('%Y-%m-%d %H:%M:%S')}'"
-#   return dataframe_query(f'''
-#   SELECT
-#     tc.DATA as 'Data',
-#     tdr.ID as 'ID_Despesa',
-#     "Nulo" as 'ID_Parcela',
-#     te.NOME_FANTASIA as 'Loja',
-#     tf.FANTASY_NAME as 'Fornecedor',
-#     tdr.VALOR_LIQUIDO as 'Valor',
-#     "Falso" as 'Parcelamento',
-#     CASE
-#         WHEN tdr.FK_STATUS_PGTO = 103 THEN 'Pago'
-#         ELSE 'Pendente'
-#     END as 'Status_Pgto'
-#   FROM T_DESPESA_RAPIDA tdr 
-#   INNER JOIN T_EMPRESAS te ON (tdr.FK_LOJA = te.ID)
-#   INNER JOIN T_FORNECEDOR tf ON (tdr.FK_FORNECEDOR = tf.ID)
-#   LEFT JOIN T_CALENDARIO tc ON (tdr.PREVISAO_PAGAMENTO = tc.ID)
-#   LEFT JOIN T_DEPESA_PARCELAS tdp ON (tdp.FK_DESPESA = tdr.ID)
-#   WHERE tdp.ID is NULL 
-#     AND tc.DATA = {dataStr}
-#   UNION ALL
-#   SELECT
-#     tc.DATA as 'Data',
-#     tdr.ID as 'ID_Despesa',
-#     tdp.ID as 'ID_Parcela',
-#     te.NOME_FANTASIA as 'Loja',
-#     tf.FANTASY_NAME as 'Fornecedor',
-#     tdp.VALOR as 'Valor',
-#     "True" as 'Parcelamento',
-#     CASE
-#         WHEN tdp.PARCELA_PAGA = 1 THEN 'Pago'
-#         ELSE 'Pendente'
-#     END as 'Status_Pgto'
-#   FROM T_DESPESA_RAPIDA tdr 
-#   INNER JOIN T_EMPRESAS te ON (tdr.FK_LOJA = te.ID)
-#   INNER JOIN T_FORNECEDOR tf ON (tdr.FK_FORNECEDOR = tf.ID)
-#   LEFT JOIN T_DEPESA_PARCELAS tdp ON (tdp.FK_DESPESA = tdr.ID)
-#   LEFT JOIN T_CALENDARIO tc ON (tdp.FK_PREVISAO_PGTO = tc.ID)
-#   WHERE tdp.ID is NOT NULL 
-#     AND tc.DATA = {dataStr}
-#     AND (tdp.PARCELA_PAGA = 0 OR tdp.PARCELA_PAGA IS NULL);
-# ''')
 
 
 def GET_DESPESAS_PENDENTES(data):

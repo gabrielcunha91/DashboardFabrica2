127353c0671c515eb71a997d168c426a5a9e61f6
LuanaFerreiraFabrica <luana.ferreira@fabricadebares.com.br>
2025-03-05 17:29:54 -0300

    Build(Despesas)

--- pages/Despesas.py
+++ pages/Despesas.py
@@ -35,16 +35,15 @@ data_inicio_default, data_fim_default = preparar_dados_datas()
 lojas_selecionadas, data_inicio, data_fim = criar_seletores(lojasComDados, data_inicio_default, data_fim_default)
 st.divider()
 
-# Despesas = GET_DESPESAS()
 despesasDetalhadas = GET_DESPESAS()
-despesasDetalhadas = filtrar_por_datas(despesasDetalhadas, data_inicio, data_fim, 'Data_Evento')
+despesasDetalhadas = filtrar_por_datas(despesasDetalhadas, data_inicio, data_fim, 'Data_Emissao')
 despesasDetalhadas = filtrar_por_classe_selecionada(despesasDetalhadas, 'Loja', lojas_selecionadas)
 
-Despesas2 = GET_DESPESAS2().drop_duplicates()
+Despesas2 = GET_DESPESAS().drop_duplicates()
 Orcamentos = GET_ORCAMENTOS_DESPESAS().drop_duplicates()
-Despesas = pd.merge(Despesas2, Orcamentos, on=['Primeiro_Dia_Mes', 'Loja', 'Plano_de_Contas', 'Class_Plano_de_Contas'], how='outer')
-Despesas['Data_Evento'] = Despesas['Data_Evento'].fillna(Despesas['Primeiro_Dia_Mes'])
-Despesas = filtrar_por_datas(Despesas, data_inicio, data_fim, 'Data_Evento')
+Despesas = pd.merge(Despesas2, Orcamentos, on=['Primeiro_Dia_Mes', 'Loja', 'Classificacao_Contabil_1', 'Classificacao_Contabil_2'], how='outer')
+Despesas['Data_Emissao'] = Despesas['Data_Emissao'].fillna(Despesas['Primeiro_Dia_Mes'])
+Despesas = filtrar_por_datas(Despesas, data_inicio, data_fim, 'Data_Emissao')
 Despesas = filtrar_por_classe_selecionada(Despesas, 'Loja', lojas_selecionadas)
 despesasConfig = config_despesas_por_classe(Despesas)
 despesasConfigStyled = despesasConfig.style.map(highlight_values, subset=['Orçamento - Realiz.'])
@@ -57,18 +56,23 @@ with st.container(border=True):
 
 despesaDetalhadaConfig = config_despesas_detalhado(despesasDetalhadas)
 
-classificacoes = preparar_dados_classe_selecionada(despesaDetalhadaConfig, 'Plano de Contas')
+classificacoes1 = preparar_dados_classe_selecionada(despesaDetalhadaConfig, 'Class. Contábil 1')
+
 
 with st.container(border=True):
   col0, col1, col2 = st.columns([1, 15, 1])
   with col1:
-    col3, col4, col5 = st.columns([2, 1, 1])
+    col3, col4, col5, col6 = st.columns([2, 1, 1, 1])
     with col3:
       st.subheader("Despesas Detalhadas:")
     with col4:
-      classificacoes_selecionadas = st.multiselect(label='Selecione Classificações', options=classificacoes)
-      despesaDetalhadaConfig = filtrar_por_classe_selecionada(despesaDetalhadaConfig, 'Plano de Contas', classificacoes_selecionadas)
+      classificacoes_1_selecionadas = st.multiselect(label='Selecione Classificação Contábil 1', options=classificacoes1)
+      despesaDetalhadaConfig = filtrar_por_classe_selecionada(despesaDetalhadaConfig, 'Class. Contábil 1', classificacoes_1_selecionadas)
     with col5:
+      classificacoes2 = preparar_dados_classe_selecionada(despesaDetalhadaConfig, 'Class. Contábil 2')
+      classificacoes_2_selecionadas = st.multiselect(label='Selecione Classificação Contábil 2', options=classificacoes2)
+      despesaDetalhadaConfig = filtrar_por_classe_selecionada(despesaDetalhadaConfig, 'Class. Contábil 2', classificacoes_2_selecionadas)
+    with col6:
       fornecedores = preparar_dados_classe_selecionada(despesaDetalhadaConfig, 'Fornecedor')
       fornecedores_selecionados = st.multiselect(label='Selecione Fornecedores', options=fornecedores)
       despesaDetalhadaConfig = filtrar_por_classe_selecionada(despesaDetalhadaConfig, 'Fornecedor', fornecedores_selecionados)
--- utils/queries.py
+++ utils/queries.py
@@ -230,98 +230,124 @@ def GET_CLSSIFICACAO():
 
 ############## Despesas ################
 
+# @st.cache_data
+# def GET_DESPESAS():
+#   return dataframe_query(f'''
+#     SELECT 
+#       tdr.ID AS ID,
+#       te.NOME_FANTASIA AS Loja,
+#       tf.CORPORATE_NAME AS Fornecedor,
+#       tdr.NF AS Doc_Serie,
+#       STR_TO_DATE(tdr.COMPETENCIA, '%Y-%m-%d') AS Data_Emissao,
+#       STR_TO_DATE(tdr.VENCIMENTO, '%Y-%m-%d') AS Data_Vencimento,
+#       tdr.OBSERVACAO AS Descricao,
+#       tdr.VALOR_LIQUIDO AS Valor_Liquido,
+#       tapdc.DESCRICAO_PLANO_DE_CONTAS AS Plano_de_Contas,
+#       tcpdc.DESCRICAO AS Class_Plano_de_Contas,
+#       to2.VALOR AS Orcamento,
+#       CASE 
+#           WHEN tdr.FK_Status = 'Provisionado' THEN 'Provisionado'
+#           ELSE 'Real'
+#       END AS Status
+#     FROM T_DESPESA_RAPIDA tdr
+#     JOIN T_EMPRESAS te ON tdr.FK_LOJA = te.ID
+#     LEFT JOIN T_FORNECEDOR tf ON tdr.FK_FORNECEDOR = tf.ID
+#     LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_2 tccg2 ON tdr.FK_CLASSIFICACAO_CONTABIL_GRUPO_2 = tccg2.ID
+#     LEFT JOIN T_ASSOCIATIVA_PLANO_DE_CONTAS tapdc ON tccg2.ID = tapdc.FK_CLASSIFICACAO_GRUPO_2
+#     LEFT JOIN T_CLASSIFICACAO_PLANO_DE_CONTAS tcpdc ON tapdc.FK_CLASSIFICACAO_PLANO_DE_CONTAS = tcpdc.ID
+#     LEFT JOIN T_ORCAMENTOS to2 ON tapdc.FK_CLASSIFICACAO_GRUPO_2 = to2.FK_CLASSIFICACAO_2 
+#       AND MONTH(CAST(tdr.COMPETENCIA AS DATE)) = to2.MES
+#       AND to2.FK_EMPRESA = te.ID
+#     WHERE tdr.FK_DESPESA_TEKNISA IS NULL
+#       AND tdr.COMPETENCIA >= '2024-01-01 00:00:00'
+#       AND tdr.VENCIMENTO >= '2024-01-01 00:00:00'
+#       AND NOT EXISTS (
+#         SELECT 1
+#         FROM T_DESPESA_RAPIDA_ITEM tdri
+#         WHERE tdri.FK_DESPESA_RAPIDA = tdr.ID);
+# ''')
+
 @st.cache_data
 def GET_DESPESAS():
-  #'Data_Evento' é, na realidade, a data da emissão, eu só coloquei esse nome pra ficar mais fácil de programar
-  #Vai mudar com a Vmarket
-  # Corrigir datas competencia e vencimento
   return dataframe_query(f'''
-    SELECT 
-      tdr.ID AS ID,
-      te.NOME_FANTASIA AS Loja,
-      tf.CORPORATE_NAME AS Fornecedor,
-      tdr.NF AS Doc_Serie,
-      STR_TO_DATE(tdr.COMPETENCIA, '%Y-%m-%d') AS Data_Evento,
-      STR_TO_DATE(tdr.VENCIMENTO, '%Y-%m-%d') AS Data_Vencimento,
-      tdr.OBSERVACAO AS Descricao,
-      tdr.VALOR_LIQUIDO AS Valor_Liquido,
-      tapdc.DESCRICAO_PLANO_DE_CONTAS AS Plano_de_Contas,
-      tcpdc.DESCRICAO AS Class_Plano_de_Contas,
-      to2.VALOR AS Orcamento,
-      CASE 
-          WHEN tdr.FK_Status = 'Provisionado' THEN 'Provisionado'
-          ELSE 'Real'
-      END AS Status
-    FROM T_DESPESA_RAPIDA tdr
-    JOIN T_EMPRESAS te ON tdr.FK_LOJA = te.ID
-    LEFT JOIN T_FORNECEDOR tf ON tdr.FK_FORNECEDOR = tf.ID
-    LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_2 tccg2 ON tdr.FK_CLASSIFICACAO_CONTABIL_GRUPO_2 = tccg2.ID
-    LEFT JOIN T_ASSOCIATIVA_PLANO_DE_CONTAS tapdc ON tccg2.ID = tapdc.FK_CLASSIFICACAO_GRUPO_2
-    LEFT JOIN T_CLASSIFICACAO_PLANO_DE_CONTAS tcpdc ON tapdc.FK_CLASSIFICACAO_PLANO_DE_CONTAS = tcpdc.ID
-    LEFT JOIN T_ORCAMENTOS to2 ON tapdc.FK_CLASSIFICACAO_GRUPO_2 = to2.FK_CLASSIFICACAO_2 
-      AND MONTH(CAST(tdr.COMPETENCIA AS DATE)) = to2.MES
-      AND to2.FK_EMPRESA = te.ID
-    WHERE tdr.FK_DESPESA_TEKNISA IS NULL
-      AND tdr.COMPETENCIA >= '2024-01-01 00:00:00'
-      AND tdr.VENCIMENTO >= '2024-01-01 00:00:00'
-      AND NOT EXISTS (
-        SELECT 1
-        FROM T_DESPESA_RAPIDA_ITEM tdri
-        WHERE tdri.FK_DESPESA_RAPIDA = tdr.ID);
+  SELECT 
+    tdr.ID AS ID,
+    te.NOME_FANTASIA AS Loja,
+    tf.CORPORATE_NAME AS Fornecedor,
+    tdr.NF AS Doc_Serie,
+    STR_TO_DATE(tdr.COMPETENCIA, '%Y-%m-%d') AS Data_Emissao,
+    STR_TO_DATE(tdr.VENCIMENTO, '%Y-%m-%d') AS Data_Vencimento,
+    CAST(DATE_FORMAT(CAST(tdr.COMPETENCIA AS DATE), '%Y-%m-01') AS DATE) AS Primeiro_Dia_Mes,
+    tdr.OBSERVACAO AS Descricao,
+    tdr.VALOR_LIQUIDO AS Valor_Liquido,
+    tccg2.DESCRICAO AS Classificacao_Contabil_2,
+    tccg1.DESCRICAO AS Classificacao_Contabil_1,
+    CASE 
+      WHEN tdr.FK_Status = 'Provisionado' THEN 'Provisionado'
+      ELSE 'Real'
+    END AS Status
+  FROM T_DESPESA_RAPIDA tdr
+  JOIN T_EMPRESAS te ON tdr.FK_LOJA = te.ID
+  LEFT JOIN T_FORNECEDOR tf ON tdr.FK_FORNECEDOR = tf.ID
+  LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_2 tccg2 ON tdr.FK_CLASSIFICACAO_CONTABIL_GRUPO_2 = tccg2.ID
+  LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_1 tccg1 ON tccg2.FK_GRUPO_1 = tccg1.ID
+  WHERE tccg1.FK_VERSAO_PLANO_CONTABIL = 103
+    AND NOT EXISTS (
+      SELECT 1
+      FROM T_DESPESA_RAPIDA_ITEM tdri
+      WHERE tdri.FK_DESPESA_RAPIDA = tdr.ID);
 ''')
 
+# @st.cache_data
+# def GET_DESPESAS():
+#   return dataframe_query(f'''
+#     SELECT 
+#       tdr.ID AS ID,
+#       te.NOME_FANTASIA AS Loja,
+#       tf.CORPORATE_NAME AS Fornecedor,
+#       tdr.NF AS Doc_Serie,
+#       STR_TO_DATE(tdr.COMPETENCIA, '%Y-%m-%d') AS Data_Evento,
+#       STR_TO_DATE(tdr.VENCIMENTO, '%Y-%m-%d') AS Data_Vencimento,
+#       CAST(DATE_FORMAT(CAST(tdr.COMPETENCIA AS DATE), '%Y-%m-01') AS DATE) AS Primeiro_Dia_Mes,
+#       tdr.OBSERVACAO AS Descricao,
+#       tdr.VALOR_LIQUIDO AS Valor_Liquido,
+#       tapdc.DESCRICAO_PLANO_DE_CONTAS AS Plano_de_Contas,
+#       tcpdc.DESCRICAO AS Class_Plano_de_Contas,
+#       CASE 
+#         WHEN tdr.FK_Status = 'Provisionado' THEN 'Provisionado'
+#         ELSE 'Real'
+#       END AS Status
+#     FROM T_DESPESA_RAPIDA tdr
+#     JOIN T_EMPRESAS te ON tdr.FK_LOJA = te.ID
+#     LEFT JOIN T_FORNECEDOR tf ON tdr.FK_FORNECEDOR = tf.ID
+#     LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_2 tccg2 ON tdr.FK_CLASSIFICACAO_CONTABIL_GRUPO_2 = tccg2.ID
+#     LEFT JOIN T_ASSOCIATIVA_PLANO_DE_CONTAS tapdc ON tccg2.ID = tapdc.FK_CLASSIFICACAO_GRUPO_2
+#     LEFT JOIN T_CLASSIFICACAO_PLANO_DE_CONTAS tcpdc ON tapdc.FK_CLASSIFICACAO_PLANO_DE_CONTAS = tcpdc.ID
+#     WHERE tdr.FK_DESPESA_TEKNISA IS NULL
+#       AND tdr.COMPETENCIA >= '2024-01-01 00:00:00'
+#       AND tdr.VENCIMENTO >= '2024-01-01 00:00:00'
+#       AND NOT EXISTS (
+#         SELECT 1
+#         FROM T_DESPESA_RAPIDA_ITEM tdri
+#         WHERE tdri.FK_DESPESA_RAPIDA = tdr.ID);
+# ''')
 
-
-@st.cache_data
-def GET_DESPESAS2():
-  return dataframe_query(f'''
-    SELECT 
-      tdr.ID AS ID,
-      te.NOME_FANTASIA AS Loja,
-      tf.CORPORATE_NAME AS Fornecedor,
-      tdr.NF AS Doc_Serie,
-      STR_TO_DATE(tdr.COMPETENCIA, '%Y-%m-%d') AS Data_Evento,
-      STR_TO_DATE(tdr.VENCIMENTO, '%Y-%m-%d') AS Data_Vencimento,
-      CAST(DATE_FORMAT(CAST(tdr.COMPETENCIA AS DATE), '%Y-%m-01') AS DATE) AS Primeiro_Dia_Mes,
-      tdr.OBSERVACAO AS Descricao,
-      tdr.VALOR_LIQUIDO AS Valor_Liquido,
-      tapdc.DESCRICAO_PLANO_DE_CONTAS AS Plano_de_Contas,
-      tcpdc.DESCRICAO AS Class_Plano_de_Contas,
-      CASE 
-        WHEN tdr.FK_Status = 'Provisionado' THEN 'Provisionado'
-        ELSE 'Real'
-      END AS Status
-    FROM T_DESPESA_RAPIDA tdr
-    JOIN T_EMPRESAS te ON tdr.FK_LOJA = te.ID
-    LEFT JOIN T_FORNECEDOR tf ON tdr.FK_FORNECEDOR = tf.ID
-    LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_2 tccg2 ON tdr.FK_CLASSIFICACAO_CONTABIL_GRUPO_2 = tccg2.ID
-    LEFT JOIN T_ASSOCIATIVA_PLANO_DE_CONTAS tapdc ON tccg2.ID = tapdc.FK_CLASSIFICACAO_GRUPO_2
-    LEFT JOIN T_CLASSIFICACAO_PLANO_DE_CONTAS tcpdc ON tapdc.FK_CLASSIFICACAO_PLANO_DE_CONTAS = tcpdc.ID
-    WHERE tdr.FK_DESPESA_TEKNISA IS NULL
-      AND tdr.COMPETENCIA >= '2024-01-01 00:00:00'
-      AND tdr.VENCIMENTO >= '2024-01-01 00:00:00'
-      AND NOT EXISTS (
-        SELECT 1
-        FROM T_DESPESA_RAPIDA_ITEM tdri
-        WHERE tdri.FK_DESPESA_RAPIDA = tdr.ID);
-''')
 
 @st.cache_data
 def GET_ORCAMENTOS_DESPESAS():
   return dataframe_query(f'''
-    SELECT
-      to2.ID AS ID_Orcamento,
-      te.NOME_FANTASIA AS Loja,
-      tapdc.DESCRICAO_PLANO_DE_CONTAS AS Plano_de_Contas,
-      tcpdc.DESCRICAO AS Class_Plano_de_Contas,
-      to2.VALOR AS Orcamento,
-      cast(date_format(cast(CONCAT(to2.ANO, '-', to2.MES, '-01') AS date), '%Y-%m-01') as date) AS Primeiro_Dia_Mes
-    FROM
-      T_ORCAMENTOS to2 
-    LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_2 tccg2 ON to2.FK_CLASSIFICACAO_2 = tccg2.ID
-    LEFT JOIN T_ASSOCIATIVA_PLANO_DE_CONTAS tapdc ON tccg2.ID = tapdc.FK_CLASSIFICACAO_GRUPO_2
-    LEFT JOIN T_CLASSIFICACAO_PLANO_DE_CONTAS tcpdc ON tapdc.FK_CLASSIFICACAO_PLANO_DE_CONTAS = tcpdc.ID
-    LEFT JOIN T_EMPRESAS te ON to2.FK_EMPRESA = te.ID;
+  SELECT
+    to2.ID AS ID_Orcamento,
+    te.NOME_FANTASIA AS Loja,
+    tccg2.DESCRICAO AS Classificacao_Contabil_2,
+    tccg1.DESCRICAO AS Classificacao_Contabil_1,
+    to2.VALOR AS Orcamento,
+    cast(date_format(cast(CONCAT(to2.ANO, '-', to2.MES, '-01') AS date), '%Y-%m-01') as date) AS Primeiro_Dia_Mes
+  FROM
+    T_ORCAMENTOS to2 
+  LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_2 tccg2 ON to2.FK_CLASSIFICACAO_2 = tccg2.ID
+  LEFT JOIN T_CLASSIFICACAO_CONTABIL_GRUPO_1 tccg1 ON tccg2.FK_GRUPO_1 = tccg1.ID
+  LEFT JOIN T_EMPRESAS te ON to2.FK_EMPRESA = te.ID;
 ''')
 
 ############################### CMV ###################################
